35ef62a7bdf4ffcbb8f3c20f2f6fe4a619ce47c7
SushanthKSR <149374015+SushanthKSR@users.noreply.github.com>
2024-04-12 14:16:49 +0530

    Add files via upload

---
+++ Find_pairs_inArr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+bool chkpair(int A[],int size, int x)
+{
+    for (int i =0; i < (size-1); i++){
+        
+         for (int j = (i + 1); j < size; j++) {
+
+            if(A[i]+A[j] == x){
+
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int A[] = {0,-1,2,4,-3,1};
+    int x = -2;
+    int size = sizeof(A) / sizeof(A[0]);
+
+if (chkpair(A, size, x)) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << x << endl;
+    }
+ 
+    return 0;
+}
